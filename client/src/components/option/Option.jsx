d22cc5130a0b0ebe0d044e373df84d7db6f89df9
Anna Dinh <annathuydinh@gmail.com>
2022-09-12 03:47:31 -0700

    option completed

--- client/src/components/option/Option.jsx
+++ client/src/components/option/Option.jsx
@@ -1,7 +1,38 @@
 import './Option.scss';
 
-function Option() {
-  return <section className="option">Option</section>;
+function Option({ option, setOption }) {
+  return (
+    <section className="option-container">
+      <section className="title">1. Select Option:</section>
+      <section className="options" onChange={(e) => setOption(e.target.value)}>
+        <section className="options-choice">
+          <section className="bold">
+            <input
+              type="radio"
+              value="gummies"
+              name="option"
+              checked={option === 'gummies'}
+            />{' '}
+            Gummies
+          </section>
+          <p>Stawberries</p>
+        </section>
+
+        <section className="options-choice">
+          <section className="bold">
+            <input
+              type="radio"
+              value="capsule"
+              checked={option === 'capsule'}
+              name="option"
+            />{' '}
+            Capsule
+          </section>
+          <p>Sugar-Free</p>
+        </section>
+      </section>
+    </section>
+  );
 }
 
 export default Option;
