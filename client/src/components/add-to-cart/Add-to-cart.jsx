ee5604573fa8387298a69c49769a8e5763af2e16
Anna Dinh <annathuydinh@gmail.com>
2022-09-12 04:44:28 -0700

    completed add to cart button

--- client/src/components/add-to-cart/Add-to-cart.jsx
+++ client/src/components/add-to-cart/Add-to-cart.jsx
@@ -1,7 +1,20 @@
 import './Add-to-cart.scss';
 
-function AddCart() {
-  return <section className="cart-button">Cart Button</section>;
+function AddCart({ oneTimePrice, subscriptionPrice, purchaseType }) {
+  const handleDisplay = () => {
+    if (purchaseType === 'subscribe & save') {
+      return (
+        <button>
+          Add to Cart - <span className="light">{oneTimePrice}</span>
+          {subscriptionPrice}
+        </button>
+      );
+    } else {
+      return <button>Add to Cart - {oneTimePrice}</button>;
+    }
+  };
+
+  return <section className="cart-button">{handleDisplay()}</section>;
 }
 
 export default AddCart;
