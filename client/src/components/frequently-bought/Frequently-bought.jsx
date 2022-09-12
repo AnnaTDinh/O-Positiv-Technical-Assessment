82303c02ccf4106e2f5d19c914afc099dd67d96f
Anna Dinh <annathuydinh@gmail.com>
2022-09-12 04:45:01 -0700

    completed optional bonus frequently bought

--- client/src/components/frequently-bought/Frequently-bought.jsx
+++ client/src/components/frequently-bought/Frequently-bought.jsx
@@ -1,7 +1,33 @@
 import './Frequently-bought.scss';
 
-function FrequentlyBought() {
-  return <section className="frequent">Frequently Bought With</section>;
+function FrequentlyBought({ frequentlyBoughtWith, setFrequentlyBoughtWith }) {
+  return (
+    <section className="frequent">
+      <section className="title">4. Frequently Purchased With:</section>
+      <section className="frequent-row">
+        <section>
+          <input
+            name="frequentlyBoughtWith"
+            type="checkbox"
+            checked={frequentlyBoughtWith}
+            onChange={() => setFrequentlyBoughtWith(!frequentlyBoughtWith)}
+          />{' '}
+        </section>
+        <section>
+          <img
+            className="disco"
+            src="https://cdn.shopify.com/s/files/1/0013/1068/7335/files/OPProducts_DISCO-1000x1000.png?v=1654901746"
+            alt="disco product"
+          ></img>
+        </section>
+        <section className="disco-text">
+          DISCO Multivitamin
+          <section className="disco-price">$26.99</section>
+          <section className="under">Learn More</section>
+        </section>
+      </section>
+    </section>
+  );
 }
 
 export default FrequentlyBought;
