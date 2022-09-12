1d2b85cfcb5b9af4626d7348d846c3af9491d284
Anna Dinh <annathuydinh@gmail.com>
2022-09-12 03:48:59 -0700

    purchase type complete

--- client/src/components/purchase-type/Purchase-type.jsx
+++ client/src/components/purchase-type/Purchase-type.jsx
@@ -1,7 +1,54 @@
 import './Purchase-type.scss';
 
-function PurchaseType() {
-  return <section className="purchase-type">Purchase Type</section>;
+function PurchaseType({
+  purchaseType,
+  setPurchaseType,
+  oneTimePrice,
+  subscriptionPrice,
+}) {
+  return (
+    <section className="purchase-type-container">
+      <section className="title">2. Purchase Type:</section>
+      <section className="purchase-type">
+        <section className="purchase-type-option bold">
+          <section>
+            <input
+              type="radio"
+              value="subscribe & save"
+              checked={purchaseType === 'subscribe & save'}
+              name="purchaseType"
+              onChange={(e) => setPurchaseType(e.target.value)}
+            />{' '}
+            Subscribe & Save
+          </section>
+          <p>Easy to cancel, anytime</p>
+          <p className="red">Free Shipping Always</p>
+        </section>
+        <section>
+          <section className="bold end">{subscriptionPrice}</section>{' '}
+          <p>/BOTTLE</p>
+        </section>
+      </section>
+      <section className="purchase-type">
+        <section className="purchase-type-option bold">
+          <section>
+            <input
+              type="radio"
+              value="one time"
+              checked={purchaseType === 'one time'}
+              name="purchaseType"
+              onChange={(e) => setPurchaseType(e.target.value)}
+            />{' '}
+            One Time
+          </section>
+          <p>One Time Purchase</p>
+        </section>
+        <section>
+          <section className="bold end">{oneTimePrice}</section> <p>/BOTTLE</p>
+        </section>
+      </section>
+    </section>
+  );
 }
 
 export default PurchaseType;
